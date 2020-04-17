d3c8c94aa1c4099ac541a4510ad0d39f9df29266
Sangmin Yoon <sangmin.yoon@croquis.com>
2020-04-17 11:19:42 +0900

    update node modules

--- packages/express/lib/logger.d.ts
+++ packages/express/lib/logger.d.ts
@@ -1,4 +1,4 @@
 import express from 'express';
 import { IExpressConfig } from './config';
-declare const _default: (config: IExpressConfig) => express.RequestHandler<import("express-serve-static-core").ParamsDictionary>;
+declare const _default: (config: IExpressConfig) => express.RequestHandler<import("express-serve-static-core").ParamsDictionary, any, any, import("express-serve-static-core").Query>;
 export default _default;
